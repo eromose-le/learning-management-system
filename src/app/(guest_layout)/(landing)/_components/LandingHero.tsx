c62db95abcc9b891ad082593c5ddd005421f07b1
eromose-le <erons24@yahoo.com>
2024-06-25 23:50:57 +0100

    fix: resolve layout bug on hero component

--- src/app/(guest_layout)/(landing)/_components/LandingHero.tsx
+++ src/app/(guest_layout)/(landing)/_components/LandingHero.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Container from "@/common/Container";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, useMediaQuery, useTheme } from "@mui/material";
 import Image from "next/image";
 import Reveals from "@/common/Reveals";
 import { FC } from "react";
@@ -26,60 +27,79 @@ const LandingHero: FC<LandingHeroProps> = ({
   imageAlt,
   inverse,
 }) => {
+  const theme = useTheme();
+  const isMd = useMediaQuery(theme.breakpoints.up("md"));
+
+  const renderImage = () =>
+    imageUrl && (
+      <div className="flex-1">
+        <Image
+          src={imageUrl}
+          alt={imageAlt || "Image"}
+          sizes="100vw"
+          layout="responsive"
+          style={{
+            width: "100%",
+            height: "auto",
+            objectFit: "cover",
+          }}
+          width={500}
+          height={300}
+        />
+      </div>
+    );
+
+  const renderSubheading = () =>
+    subheading && (
+      <div className="flex-1 mt-2 md:mt-0">
+        <Typography variant="h6" className="font-normal text-tertiary">
+          {subheading}
+        </Typography>
+      </div>
+    );
+
+    const renderButtons = () =>
+      showButtons && (
+        <div className="flex flex-col gap-6 md:flex-row mt-7 md:mt-9 md:gap-3">
+          <Button variant="contained" size="large" className="px-10">
+            {buttonPrimaryText}
+          </Button>
+          <Button variant="linkSecondary" size="large">
+            {buttonSecondaryText}
+          </Button>
+        </div>
+      );
+
   return (
     <Container
       component="section"
-      className={clsx(inverse && "md:items-end", "flex flex-col-reverse justify-between gap-8 m-auto mt-0 md:flex-row md:mt-24 md:gap-14 max-w-screen-2xl")}
+      className={clsx(
+        inverse && "md:items-end flex-col-reverse",
+        "flex flex-col-reverse justify-between gap-8 m-auto mt-0 md:flex-row md:mt-24 md:gap-14 max-w-screen-2xl"
+      )}
     >
       <div className="flex-1">
-        <div className="flex flex-col max-w-md gap-6 mt-6 text-center md:text-start md:gap-8 md:mt-0">
+        <div className="flex flex-col max-w-md gap-8 mt-6 text-center md:text-start md:gap-8 md:mt-0">
           <Reveals>
             <Typography variant="h1" className="font-semibold text-secondary">
               {heading}
             </Typography>
           </Reveals>
-          {!inverse && subheading && (
-            <Typography variant="h6" className="font-normal text-tertiary">
-              {subheading}
-            </Typography>
-          )}
+          {isMd
+            ? !inverse
+            : inverse &&
+              subheading && (
+                <Typography variant="h6" className="font-normal text-tertiary">
+                  {subheading}
+                </Typography>
+              )}
         </div>
 
-        {showButtons && (
-          <div className="flex flex-col gap-6 md:flex-row mt-7 md:mt-9 md:gap-3">
-            <Button variant="contained" size="large" className="px-10">
-              {buttonPrimaryText}
-            </Button>
-            <Button variant="linkSecondary" size="large">
-              {buttonSecondaryText}
-            </Button>
-          </div>
-        )}
+        {renderButtons()}
       </div>
-      {imageUrl && (
-        <div className="flex-1">
-          <Image
-            src={imageUrl}
-            alt={imageAlt || "Image"}
-            sizes="100vw"
-            style={{
-              width: "100%",
-              height: "auto",
-              objectFit: "cover",
-            }}
-            width={500}
-            height={300}
-          />
-        </div>
-      )}
 
-      {inverse && subheading && (
-        <div className="flex-1">
-          <Typography variant="h6" className="font-normal text-tertiary">
-            {subheading}
-          </Typography>
-        </div>
-      )}
+      {renderImage()}
+      {isMd && inverse && renderSubheading()}
     </Container>
   );
 };
