7c5c725b7cf4fc2ae1363edf183707aaeb60a8f8
eromose-le <erons24@yahoo.com>
2024-07-11 20:15:47 +0100

    chore: fix alignment

--- src/app/(guest_layout)/(landing)/_components/LandingDescriptionDesktop.tsx
+++ src/app/(guest_layout)/(landing)/_components/LandingDescriptionDesktop.tsx
@@ -4,7 +4,7 @@ const LandingDescriptionDesktop = ({
   cards: React.ReactNode[];
 }) => {
   return (
-    <div className="flex items-center justify-center flex-wrap gap-9 w-full">
+    <div className="flex items-center justify-center lg:justify-between flex-wrap gap-9 w-full">
       {cards.map((card, index) => (
         <div key={index}>{card}</div>
       ))}
